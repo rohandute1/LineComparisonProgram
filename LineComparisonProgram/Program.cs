15149d996e24b421d35b49fd1ce6bbfc302daa6d
Rohan <rohandute1@gmail.com>
2023-05-11 15:49:56 +0530

    [Rohan] Modify Calculate line length

--- LineComparisonProgram/Program.cs
+++ LineComparisonProgram/Program.cs
@@ -10,8 +10,25 @@ namespace LineComparisonProgram
     {
         static void Main(string[] args)
         {
+            // Initialize variables
+            int x1, y1, x2, y2, deltaX, deltaY, lengthSquared, length;
             Console.WriteLine("Welcome to Line Comparison Program");
+            // Input two points
+            Console.WriteLine("Enter the first point (x1, y1):");
+            x1 = int.Parse(Console.ReadLine());
+            y1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second point (x2, y2):");
+            x2 = int.Parse(Console.ReadLine());
+            y2 = int.Parse(Console.ReadLine());
+            // Calculate the length of the line
+            deltaX = x2 - x1;
+            deltaY = y2 - y1;
+            lengthSquared = deltaX * deltaX + deltaY * deltaY;
+            length = (int)Math.Sqrt(lengthSquared);
+            // Output the result
+            Console.WriteLine($"The length of the line is {length}");
             Console.ReadLine();
         }
     }
+
 }
