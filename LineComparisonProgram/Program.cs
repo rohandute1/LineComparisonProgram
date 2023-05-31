0aa8b6694830666b1a42a18c3c7bb4860b93666b
Rohan <rohandute1@gmail.com>
2023-05-31 23:45:14 +0530

    [Rohan] Comparing length of two lines

--- LineComparisonProgram/Program.cs
+++ LineComparisonProgram/Program.cs
@@ -6,44 +6,80 @@ using System.Threading.Tasks;
 
 namespace LineComparisonProgram
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            int x1, y1, x2, y2, a1, b1, a2, b2;
             Console.WriteLine("Welcome to Line Comparison Program");
-            Console.WriteLine("Enter the first line coordinates (x1, y1, x2, y2):");
-            x1 = Convert.ToInt32(Console.ReadLine());
-            y1 = Convert.ToInt32(Console.ReadLine());
-            x2 = Convert.ToInt32(Console.ReadLine());
-            y2 = Convert.ToInt32(Console.ReadLine());
-            int length1 = CalculateLength(x1, y1, x2, y2);
-            Console.WriteLine("Enter the second line coordinates (a1, b1, a2, b2):");
-            a1 = Convert.ToInt32(Console.ReadLine());
-            b1 = Convert.ToInt32(Console.ReadLine());
-            a2 = Convert.ToInt32(Console.ReadLine());
-            b2 = Convert.ToInt32(Console.ReadLine());
-            int length2 = CalculateLength(a1, b1, a2, b2);
 
-            if (length1 == length2)
+            Console.WriteLine("Enter the first line coordinates (x1, y1, x2, y2):");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            Line line1 = new Line(new Point(x1, y1), new Point(x2, y2));
+
+            Console.WriteLine("Enter the second line coordinates (a1, b1, a2, b2):");
+            int a1 = Convert.ToInt32(Console.ReadLine());
+            int b1 = Convert.ToInt32(Console.ReadLine());
+            int a2 = Convert.ToInt32(Console.ReadLine());
+            int b2 = Convert.ToInt32(Console.ReadLine());
+            Line line2 = new Line(new Point(a1, b1), new Point(a2, b2));
+
+            int comparisonResult = line1.CompareTo(line2);
+
+            if (comparisonResult == 0)
             {
                 Console.WriteLine("The two lines are equal in length.");
             }
-            else if(length1>length2)
+            else if (comparisonResult > 0)
             {
-                Console.WriteLine("The first line is greater than second .");
+                Console.WriteLine("The first line is greater than the second line.");
             }
             else
             {
-                Console.WriteLine("The first line is shorter than second .");
+                Console.WriteLine("The first line is shorter than the second line.");
             }
+
             Console.ReadLine();
         }
+    }
 
-        static int CalculateLength(int x1, int y1, int x2, int y2)
+    class Point
+    {
+        public int X { get; }
+        public int Y { get; }
+
+        public Point(int x, int y)
         {
-            int deltaX = x2 - x1;
-            int deltaY = y2 - y1;
+            X = x;
+            Y = y;
+        }
+    }
+
+    class Line : IComparable<Line>
+    {
+        public Point StartPoint { get; }
+        public Point EndPoint { get; }
+
+        public Line(Point startPoint, Point endPoint)
+        {
+            StartPoint = startPoint;
+            EndPoint = endPoint;
+        }
+
+        public int CompareTo(Line other)
+        {
+            int length1 = CalculateLength();
+            int length2 = other.CalculateLength();
+
+            return length1.CompareTo(length2);
+        }
+
+        private int CalculateLength()
+        {
+            int deltaX = EndPoint.X - StartPoint.X;
+            int deltaY = EndPoint.Y - StartPoint.Y;
             int lengthSquared = deltaX * deltaX + deltaY * deltaY;
             int length = (int)Math.Sqrt(lengthSquared);
             return length;
